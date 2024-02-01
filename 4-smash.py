19f60ca89aa784ad6f890dcd6bce6cf7fa0a2616
arthackrc <satriadpnusantara@gmail.com>
2024-02-02 02:49:39 +0700

    day 4/366

---
+++ 4-smash.py
@@ -0,0 +1,8 @@
+# WPU Coding Challenge 2024 But in Python
+# 4/366
+# https://www.codewars.com/kata/53dc23c68a0c93699800041d
+
+def smash(words):
+    return ' '.join(words)
+
+print(smash(['this', 'is', 'a', 'really', 'long', 'sentence']))
